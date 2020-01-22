2122fa8d88d80e937300956ac143df5748700830
CaiCoding2 <caicoding2@gmail.com>
2020-01-22 09:56:34 -0600

    Update Index and remove unuse file

--- Project_0/Bank3.0/src/com/bank/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.bank.dao;
-
-import com.bank.to.Customer;
-import com.bank.exception.BusinessException;
-
-public interface CustomerDao {
-	public Customer addCustInfo(Customer cust) throws BusinessException;
-	public Customer getCustInfo(int id) throws BusinessException;
-}
--- Project_0/Bank3.0/src/com/bank/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.bank.dao;
-
-import com.bank.to.User;
-import com.bank.exception.BusinessException;
-
-public interface UserDao {
-	public boolean createUser(User user) throws BusinessException;
-	public User authenticateUser(String user, String password) throws BusinessException;
-}
